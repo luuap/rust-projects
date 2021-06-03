c219cb233ade1b9f46548aa0e4a1df1ca8e41483
luuap <35545769+luuap@users.noreply.github.com>
2021-06-03 00:11:41 -0400

    refactor(tic-tac-toe): clearer alpha-beta pruning, simpler find_best_move

--- packages/tic-tac-toe/src/lib.rs
+++ packages/tic-tac-toe/src/lib.rs
@@ -75,12 +75,6 @@ impl PlayerType {
     }
   }
 
-  // pub fn winning_state(&self) -> GameState {
-  //   match self {
-  //     Self::Cross => GameState::CrossWins,
-  //     Self::Nought => GameState::NoughtWins,
-  //   }
-  // }
 }
 
 #[derive(Copy, Clone)]
@@ -253,51 +247,59 @@ pub struct TicTacToeAI {}
 impl TicTacToeAI {
 
   pub fn find_best_move(current_player: PlayerType, game: TicTacToe) -> u32 {
-    let mut scores = Vec::<(u32, i32)>::new();
 
     let board = game.noughts() | game.crosses();
     let mut mask = TicTacToe::INITIAL_MASK;
-    // for each possible move
-    for i in 0..9 {
-      
-      // Make a copy of the game
-      let mut next_game = game;
+    let mut best_move: Option<u32> = None;
 
-      match current_player {
-        PlayerType::Nought => next_game.set_nought(i),
-        PlayerType::Cross => next_game.set_cross(i),
-      }
-
-      // check if index is available
-      if (board & mask) == 0 {
-        let next_player = current_player.switch_player();
-        scores.push((i, Self::minimax(next_player, next_game, 0, std::i32::MIN, std::i32::MAX)));
-      }
-
-      mask >>= 1;
-    }
-
-    let best_move_opt = match current_player {
+    match current_player {
       PlayerType::Nought => {
-        scores.into_iter().min_by(|(_, x), (_, y)| x.cmp(y)).map(|v| v.0)
+        let mut best_score = std::i32::MAX;
+        for i in 0..9 {
+          let mut next_game = game;
+          next_game.set_nought(i);
+          if (board & mask) == 0 {
+            let next_player = current_player.switch_player();
+            // alpha is initially set as the worst score for maximizer, beta is set to the worst score for minimizer
+            let new_score = Self::minimax(next_player, next_game, std::i32::MIN, std::i32::MAX);
+            if new_score < best_score {
+              best_score = new_score;
+              best_move = Some(i);
+            }
+          }
+          mask >>= 1;
+        }
       },
       PlayerType::Cross => {
-        scores.into_iter().max_by(|(_, x), (_, y)| x.cmp(y)).map(|v| v.0)
+        let mut best_score = std::i32::MIN;
+        for i in 0..9 {
+          let mut next_game = game;
+          next_game.set_nought(i);
+          if (board & mask) == 0 {
+            let next_player = current_player.switch_player();
+            let new_score = Self::minimax(next_player, next_game, std::i32::MIN, std::i32::MAX);
+            if new_score > best_score {
+              best_score = new_score;
+              best_move = Some(i);
+            }
+          }
+          mask >>= 1;
+        }
       },
-    };
+    }
 
-    if let Some(best_move) = best_move_opt {
+    if let Some(best_move) = best_move {
       best_move as u32
     } else {
       panic!("No moves left, reached invalid game state")
     }
   }
 
-  pub fn minimax(current_player: PlayerType, game: TicTacToe, score: i32, mut alpha: i32, mut beta: i32) -> i32 {
-
+  pub fn minimax(current_player: PlayerType, game: TicTacToe, alpha: i32, beta: i32) -> i32 {
+    use std::cmp::{min, max};
     match game.state() {
-      GameState::NoughtWins(_) => score - 1,
-      GameState::CrossWins(_) => score + 1,
+      GameState::NoughtWins(_) => -1,
+      GameState::CrossWins(_) => 1,
       GameState::Draw => 0,
       GameState::InProgress => {
 
@@ -321,21 +323,16 @@ impl TicTacToeAI {
               // check if index is available
               if (board & mask) == 0 {
 
-                let new_score = Self::minimax(next_player, next_game, score, alpha, beta);
-                
-                // determine min
-                if new_score < best_score {
-                  best_score = new_score;
-                }
+                // minimize best score
+                // Note: pass down best_score as beta
+                best_score = min(best_score, Self::minimax(next_player, next_game, alpha, best_score));
 
-                if beta < best_score {
-                  beta = best_score
-                }
-
-                if beta <= alpha {
+                // If the best score for this node (minimizer) is less than or equal to alpha (the best value among previously searched siblings for the parent (maximizer)), stop searching the rest of the children.
+                // Once best_score is less than or equal to alpha, it is guaranteed that the parent (maximizer) will never choose the value of this node, 
+                // because this node (minimizer) will never choose anything higher than the current best_score (reacll that higher is better for parent).
+                if best_score <= alpha {
                   break
                 }
-
               }
 
               mask >>= 1;
@@ -351,16 +348,18 @@ impl TicTacToeAI {
               let mut next_game = game;
               next_game.set_cross(i);
               if (board & mask) == 0 {
-                let new_score = Self::minimax(next_player, next_game, score, alpha, beta);
-                if new_score > best_score {
-                  best_score = new_score;
-                }
-                if alpha > best_score {
-                  alpha = best_score
-                }
-                if beta <= alpha {
+
+                // maximize the best_score
+                // Note: pass down best_score as alpha
+                best_score = max(best_score, Self::minimax(next_player, next_game, best_score, beta));
+
+                // If the best score for this node (maximizer) is greater than or equal to beta (the best value among previously searched siblings for the parent (minimizer)), stop searching the rest of the children.
+                // Once best_score is greater than or equal to beta, it is guaranteed that the parent (minimizer) will never choose the value of this node, 
+                // because this node (maximizer) will never choose anything lower than the current best_score (recall that lower is better for parent).
+                if best_score >= beta {
                   break
                 }
+
               }
               mask >>= 1;
             }
